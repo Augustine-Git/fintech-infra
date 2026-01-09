13f5219dfedf397f3e66fa15f833d282245abebf
Shollz <augustineogunlade1@gmail.com>
2026-01-09 07:34:21 +0000

    updated kubernetes data

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -2,21 +2,22 @@
 # EKS Data + Auth
 ##############################################
 
-data "aws_eks_cluster_auth" "main" {
+data "aws_eks_cluster" "this" {
   name = module.eks.cluster_name
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(
+    data.aws_eks_cluster.this.certificate_authority[0].data
+  )
+  token                  = data.aws_eks_cluster_auth.this.token
 }
+
 
 ##############################################
 # EKS Control Plane + Node Groups + Add-ons
