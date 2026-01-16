a8fbb1c4bb8ff9faa9e0a7ec18387e9834f42831
Shollz <augustineogunlade1@gmail.com>
2026-01-16 21:47:04 +0000

    updated cluster name

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -2,13 +2,17 @@
 # EKS Data + Auth
 ##############################################
 
-data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
 
 provider "kubernetes" {
   host = data.aws_eks_cluster.this.endpoint
